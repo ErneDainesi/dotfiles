c9c9d479374b8c4a5d8f57af1c8938b2d53bf720
ErneDainesi <edainesi@fi.uba.ar>
2023-02-03 19:51:54 -0300

    add go to definition at on_attach function of lsp

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -23,6 +23,8 @@ lsp.defaults.cmp_mappings({
 
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 end)
 
