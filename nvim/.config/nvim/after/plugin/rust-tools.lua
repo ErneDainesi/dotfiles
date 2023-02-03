15be40150dddb753da55f1c299ffd6524e4f5e11
ErneDainesi <edainesi@fi.uba.ar>
2023-02-03 19:51:33 -0300

    fix go to definition on rust

--- nvim/.config/nvim/after/plugin/rust-tools.lua
+++ nvim/.config/nvim/after/plugin/rust-tools.lua
@@ -1,11 +1,13 @@
 local rt = require("rust-tools")
 
 rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      vim.keymap.set("n", "<C-h>", rt.hover_actions.hover_actions, { buffer = bufnr }) -- Hover actions
-    end,
-  },
+    server = {
+        on_attach = function(_, bufnr)
+            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, { buffer = bufnr })
+            vim.keymap.set("n", "<C-h>", rt.hover_actions.hover_actions, { buffer = bufnr }) -- Hover actions
+            vim.keymap.set("n", "<Leader>ag", rt.code_action_group.code_action_group, { buffer = bufnr })
+        end,
+    },
 })
 -- Set inlay hints for the current buffer
 rt.inlay_hints.set()
