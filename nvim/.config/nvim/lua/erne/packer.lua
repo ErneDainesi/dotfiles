e3752908191787a11a2fd87e4395198db7bbd10f
ErneDainesi <edainesi@fi.uba.ar>
2023-02-05 15:11:39 -0300

    add lualine plugin

--- nvim/.config/nvim/lua/erne/packer.lua
+++ nvim/.config/nvim/lua/erne/packer.lua
@@ -39,4 +39,8 @@ return require('packer').startup(function(use)
     }
     use 'simrat39/rust-tools.nvim'
     use "lewis6991/gitsigns.nvim"
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
 end)
