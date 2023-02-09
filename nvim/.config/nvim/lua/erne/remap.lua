c404f10cad54d0293c7e4a63d653750de712b079
ErneDainesi <edainesi@fi.uba.ar>
2023-02-09 17:39:49 -0300

    add remap for horizontal split

--- nvim/.config/nvim/lua/erne/remap.lua
+++ nvim/.config/nvim/lua/erne/remap.lua
@@ -8,6 +8,7 @@ vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<A-v>", "<C-w>v")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
