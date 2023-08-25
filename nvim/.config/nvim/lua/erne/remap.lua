a8925aa1f7485f61dce1565a0ac712e0d2e36916
ErneDainesi <edainesi@fi.uba.ar>
2023-08-24 21:45:29 -0300

    add copy path function

--- nvim/.config/nvim/lua/erne/remap.lua
+++ nvim/.config/nvim/lua/erne/remap.lua
@@ -1,4 +1,14 @@
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+function CopyPath()
+    local filename = vim.fn.expand('%')
+    local cmd = string.format("echo %s | xclip -selection clipboard", filename)
+    if os.execute(cmd) then
+        print(string.format("Copied path: %s", filename))
+    else
+        print("Failed to copy pwd")
+    end
+end
+
 vim.keymap.set("n", "<leader>h", vim.cmd.nohlsearch)
 vim.keymap.set("n", "gl", function() vim.diagnostic.open_float(0, { scope = "line", border = "rounded" }) end)
 vim.keymap.set("n", "<Space>H", function() vim.lsp.buf.hover() end)
@@ -10,6 +20,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<A-v>", "<C-w>v")
 vim.keymap.set("n", "<Space>gl", ":lua require'gitsigns'.blame_line()<CR>")
+vim.keymap.set("n", "<Space>cp", function() CopyPath() end)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
