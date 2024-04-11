a886b26131601c6d3db7883e5df8c5e69eb2ac09
ErneDainesi <edainesi@fi.uba.ar>
2024-04-11 15:26:29 -0300

    add telescope lsp references remap

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -6,6 +6,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- Find all files
 vim.keymap.set('n', '<leader>pf', builtin.git_files, {})  -- Find files of git repo
 vim.keymap.set('n', '<leader>F', builtin.live_grep, {})   -- Find text in files
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})     -- Find files in opened buffers
+vim.keymap.set('n', '<leader>r', builtin.lsp_references, {})     -- Find files in opened buffers
 
 telescope.setup {
     defaults = {
