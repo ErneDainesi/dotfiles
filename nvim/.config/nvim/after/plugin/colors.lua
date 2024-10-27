be5ab67177dbaedd1c1beb5404b6e2b3af9c7946
ErneDainesi <edainesi@fi.uba.ar>
2024-10-27 14:58:37 -0300

    add condition to set bg transparency

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,8 +1,10 @@
-function ColorMyCode(color)
+function ColorMyCode(color, bgt)
     color = color or "onedark"
     vim.cmd.colorscheme(color)
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    if bgt then
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end
 end
 
 require("catppuccin").setup({
@@ -69,5 +71,32 @@ require("onedarkpro").setup({
     }
 })
 
---ColorMyCode('rose-pine')
-ColorMyCode()
+-- Default options:
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "hard", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
+})
+
+-- ColorMyCode('rose-pine', false)
+ColorMyCode('onedark', false)
+-- ColorMyCode('gruvbox', false)
